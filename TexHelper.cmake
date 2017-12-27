694367e56f4b5c9127e8393b388a75facf7d9e82
Stephen Newell <sjnewell@gmail.com>
2017-12-26 18:22:47 -0700

    Use functions instead of macros where possible

--- TexHelper.cmake
+++ TexHelper.cmake
@@ -9,30 +9,31 @@ function   (trace_message message)
     endif(TEXHELPER_TRACING)
 endfunction(trace_message)
 
-macro   (wrap_tex_wrapper tool output target_file dependency)
+function(wrap_tex_wrapper tool output target_file dependency)
     add_custom_command(OUTPUT ${output}
                        COMMAND ${SH} ${build_script} ${CMAKE_CURRENT_SOURCE_DIR} ${tool} ${target_file}
                        COMMAND ${TOUCH} ${output}
                        DEPENDS ${dependency})
-endmacro(wrap_tex_wrapper)
+endfunction(wrap_tex_wrapper)
 
-macro   (create_tex_output tool output root_name dependency)
+function(create_tex_output tool output root_name dependency)
     wrap_tex_wrapper(${tool} ${output} "${root_name}.tex" "${dependency}")
-endmacro(create_tex_output)
+endfunction(create_tex_output)
 
-macro   (create_aux_output tool output root_name dependency)
+function(create_aux_output tool output root_name dependency)
     wrap_tex_wrapper(${tool} ${output} "${root_name}" "${dependency}")
-endmacro(create_aux_output)
+endfunction(create_aux_output)
 
 macro   (build_create tool)
     find_program(path_${tool} ${tool})
     if   (path_${tool})
         trace_message("Found ${tool} (${path_${tool}})")
         trace_message("Adding create_tex_output_${tool}")
-        macro   (create_tex_output_${tool} output root dependency)
+        function(create_tex_output_${tool} output root dependency)
             create_tex_output(${${path}_${tool}} ${path_${tool}} ${output} ${root} "${dependency}")
-        endmacro(create_tex_output_${tool})
+        endfunction(create_tex_output_${tool})
     endif(path_${tool})
+    unset(path_${tool})
 endmacro(build_create)
 
 macro   (build_aux tool)
@@ -44,6 +45,7 @@ macro   (build_aux tool)
             create_aux_output(${${path}_${tool}} ${path_${tool}} ${output} ${root} "${dependency}")
         endmacro(create_aux_output_${tool})
     endif(path_${tool})
+    unset(path_${tool})
 endmacro(build_aux)
 
 build_create("lualatex")
